c3cbc22df14fc052adbff77be36593b5022139ff
Alex Marks <71220360+alexpmarks7@users.noreply.github.com>
2021-02-09 11:43:29 +0000

    Wrote functionality in method and added further test case

--- Raindrops/RaindropsTests/RaindropsTests/Raindrops.cs
+++ Raindrops/RaindropsTests/RaindropsTests/Raindrops.cs
@@ -2,7 +2,28 @@
 {
 	public class Raindrops
 	{
+		private string _response = "";
 
+		public string PlingPlangPlong(int n)
+		{
+			if (n % 3 == 0)
+			{
+				_response += "Pling";
+			}
+			if (n % 5 == 0)
+			{
+				_response += "Plang";
+			}
+			if (n % 7 == 0)
+			{
+				_response += "Plong";
+			}
+			if (_response == "")
+			{
+				_response += $"{n}";
+			}
+			return _response;
+		}
 
 	}
 }
--- Raindrops/RaindropsTests/RaindropsTests/RaindropsTests.cs
+++ Raindrops/RaindropsTests/RaindropsTests/RaindropsTests.cs
@@ -4,13 +4,28 @@ namespace RaindropsTests
 {
 	public class Tests
 	{
+		private Raindrops _raindrops;
+
 		[SetUp]
-		public void Setup() { }
-
-		[Test]
-		public void UnitTest1()
+		public void Setup()
 		{
+			_raindrops = new Raindrops();
+		}
 
+		[TestCase(1, "1")]
+		[TestCase(0, "PlingPlangPlong")]
+		[TestCase(34, "34")]
+		[TestCase(3, "Pling")]
+		[TestCase(5, "Plang")]
+		[TestCase(7, "Plong")]
+		[TestCase(15, "PlingPlang")]
+		[TestCase(21, "PlingPlong")]
+		[TestCase(35, "PlangPlong")]
+		[TestCase(105, "PlingPlangPlong")]
+		[TestCase(-3, "Pling")]
+		public void GivenIGiveAnInteger_AndICheckItsFactors_IGetTheCorrectPlingPlangPlongResponse(int input, string expected)
+		{
+			Assert.That(_raindrops.PlingPlangPlong(input), Is.EqualTo(expected));
 		}
 	}
 }
