63f6455d5a2f01b4d8c2c1bfd413f2303e8f5f4b
Alex Marks <71220360+alexpmarks7@users.noreply.github.com>
2021-02-09 11:37:54 +0000

    Created Project

---
+++ Raindrops/RaindropsTests/Raindrops/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace Raindrops
+{
+	public class Program
+	{
+		private static List<int> _factors;
+		private static string _response = "";
+
+		public static void Main(string[] args)
+		{
+
+		}
+
+		public string PlingPlangPlong(int n)
+		{
+			_factors = Factors(n);
+			if (_factors.Contains(3))
+			{ 
+				_response += "Pling";				
+			}
+			if (_factors.Contains(5))
+			{
+				_response += "Plang";
+			}
+			if (_factors.Contains(7))
+			{
+				_response += "Plong";
+			}
+			else
+			{
+				_response = $"{n}";
+			}
+			return _response;
+		}
+
+		public static List<int> Factors(int n)
+		{
+			for (int i = 1; i <= n; i++)
+			{
+				if (n % i == 0) _factors.Add(i);
+			}
+			return _factors;
+		}
+	}
+}
